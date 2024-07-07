81658aee2ab85311ce1b7b61698c5f637aff6880
arduini-eduarda <132152352+arduini-eduarda@users.noreply.github.com>
2024-07-07 16:17:37 -0300

    Update Lendo_csv.py

--- material/Lendo_csv.py
+++ material/Lendo_csv.py
@@ -1,12 +1,5 @@
-import csv
+import csv #'importamos o modulo do python para trabalhar com csv
 
-with open('Temperatura.csv', 'r') as csvfile:
-    leitor = csv.reader(csvfile)
-    for linha in leitor:
-        print(linha)
-
-
-'''importamos o modulo do python para trabalhar com csv'''
 
 '''usamos o with para usar um arquivo e fechar esse arquivo quando terminar o que 
 tiver dentro, mesmo se ocorrer um erro durante a execução do bloco de código. '''
@@ -18,7 +11,14 @@ tiver dentro, mesmo se ocorrer um erro durante a execução do bloco de código.
 '''Esta parte da linha cria um apelido (ou referência) chamado csvfile que 
 será usado para se referir ao arquivo aberto dentro do bloco with'''
 
-'''Usamos a função csv reader para ler cada linha do arquivo'''
+with open('Temperatura.csv', 'r') as csvfile: 
+    leitor = csv.reader(csvfile) #Usamos a função csv reader para criar nosso leitor
+    for linha in leitor: #aqui fazemos um for para ler cada linha do arquivo
+        print(linha)
+
+
+
+
 
 
     
